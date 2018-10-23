cd1ae8e9b55a991aba20a94ff5367821f33bbdf2
mathebulaF95 <mathebulaF95@gmail.com>
2018-10-23 10:59:12 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  name;
+  surname;
+  email;
+  username;
+  password;
+  confirmPassword;
 
+  registering : boolean = false;
+  log : boolean = false;
+  info : boolean = false;
+  logInfo : boolean = false;
+
+  constructor(public navCtrl: NavController) {
+  this.registering = true;
+  this.log = true;
   }
+
+  register(){
+    this.registering = false;
+   this.info = true;
+  }
+login(){
+this.log = false;
+ this.logInfo = true;
+}
+
+submit(){
+  this.info = false;
+  this.registering = true;
+}
 
 }
